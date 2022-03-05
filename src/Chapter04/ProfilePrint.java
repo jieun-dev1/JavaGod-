eb6efcfcbe8a1fa23730d1221f65ad2b64b371a2
jieun-dev1 <jkdev1324@gmail.com>
2022-03-05 23:55:42 +0900

    4장 과제

---
+++ src/Chapter04/ProfilePrint.java
@@ -0,0 +1,43 @@
+package Chapter04;
+
+public class ProfilePrint {
+	byte age;
+	String name;
+	Boolean isMarried;
+
+	public void setAge(byte paramAge) {
+		age = paramAge;
+	}
+
+	public byte getAge() {
+		return age;
+	}
+
+	public void setName(String paramName) {
+		name = paramName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setMarried(boolean flag) {
+		isMarried = flag;
+	}
+
+	public Boolean isMarried() {
+		return isMarried;
+	}
+
+	public static void main(String[] args) {
+		ProfilePrint prof = new ProfilePrint();
+		prof.setAge((byte) 10);
+		prof.setName("jk");
+		prof.setMarried((boolean) false);
+		System.out.println("My age is " + prof.getAge());
+		System.out.println("My Name is " + prof.getName());
+		System.out.println("My Marriage is " + prof.isMarried());
+
+	}
+
+}
