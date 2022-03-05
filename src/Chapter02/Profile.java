25480afc647df6c15bbb499b0b435e97de89e723
jieun-dev1 <jkdev1324@gmail.com>
2022-03-05 17:07:51 +0900

    ch2 과제

---
+++ src/Chapter02/Profile.java
@@ -0,0 +1,13 @@
+package Chapter02;
+
+public class Profile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "jieun";
+		int age = 10;
+		System.out.println("My name is " + name);
+		System.out.println("My age is " + age);
+	}
+
+}
