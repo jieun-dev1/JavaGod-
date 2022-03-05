588f06a169854b17c68d75a1eb87768f2e0724fa
jieun-dev1 <jkdev1324@gmail.com>
2022-03-05 21:53:28 +0900

    연습문제

---
+++ src/Chapter03/Profile.java
@@ -0,0 +1,36 @@
+package Chapter03;
+
+public class Profile {
+	String name;
+	int age;
+
+	public void setName(String str) {
+		name = str;
+	}
+
+	public void setAge(int val) {
+		age = val;
+	}
+
+	public void printName() {
+		System.out.println("My name is " + name);
+	}
+
+	public void printAge() {
+		System.out.println("My age is " + age);
+
+	}
+
+	public static void main(String[] args) {
+		Profile profile = new Profile();
+		profile.setName("Min");
+		profile.setAge(20);
+		profile.printName();
+		profile.printAge();
+
+//		System.out.println("My name is " + profile.name);
+//		System.out.println("My age is " + profile.age);
+
+	}
+
+}
